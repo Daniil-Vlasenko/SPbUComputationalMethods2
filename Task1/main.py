b66ea78883966845c04801da4b7ec1e7e578a61a
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2022-09-26 18:48:23 +0300

    .

--- Task1/main.py
+++ Task1/main.py
@@ -10,10 +10,32 @@ def fFunction(x, y):
     return -12 * x * y * (y ** 2 + x ** 2)
 
 
-def terminationMethod(fistApproximation, lastApproximation, uFunctionGrid, eps):
-    return np.max(np.abs(lastApproximation - uFunctionGrid)) / np.max(np.abs(fistApproximation - uFunctionGrid)) < eps
+def terminationMethod(fistApproximation, lastApproximation, uGrid, eps):
+    return np.max(np.abs(lastApproximation - uGrid)) / np.max(np.abs(fistApproximation - uGrid)) < eps
 
 
+def uGrid(xNods, yNods):
+    return np.array([[uFunction(i / xNods, j / yNods) for j in range(yNods + 1)] for i in range(xNods + 1)])
+
+
+def fGrid(xNods, yNods):
+    return np.array([[fFunction(i / xNods, j / yNods) for j in range(yNods + 1)] for i in range(xNods + 1)])
+
+
+def LuGrid(uApproximation, step):
+    yNods = len(uApproximation) - 1
+    xNods = len(uApproximation[0]) - 1
+    LuResultGrid = np.zeros((yNods + 1, xNods + 1))
+    for i in range(1, yNods):
+        for j in range(1, xNods):
+            LuResultGrid[i][j] = (uApproximation[i + 1][j] - uApproximation[i][j]) / (step ** 2) - \
+                                 (uApproximation[i][j] - uApproximation[i - 1][j]) / (step ** 2) + \
+                                 (uApproximation[i][j + 1] - uApproximation[i][j]) / (step ** 2) - \
+                                 (uApproximation[i][j] - uApproximation[i][j - 1]) / (step ** 2)
+    return LuResultGrid
+
+
+# -------------------------------------------------------------------------------------------------------------------- #
 eps = 0.001
 xlength, yLength = 1, 1
 xNods, yNods = 3, 3
@@ -22,21 +44,20 @@ xStep, yStep = xlength / xNods, yLength / yNods
 step = xStep
 numberOfIterations = 0
 
-uFunctionGrid = np.array([[uFunction(i / xNods, j / yNods) for j in range(yNods + 1)] for i in range(xNods + 1)])
-fFunctionGrid = np.array([[fFunction(i / xNods, j / yNods) for j in range(yNods + 1)] for i in range(xNods + 1)])
+uGrid = uGrid(xNods, yNods)
+fFunctionGrid = fGrid(xNods, yNods)
 
 oldApproximation = np.zeros((yNods + 1, xNods + 1))
 for j in range(xNods + 1):
-    oldApproximation[0][j] = uFunctionGrid[0][j]
-    oldApproximation[yNods][j] = uFunctionGrid[yNods][j]
+    oldApproximation[0][j] = uGrid[0][j]
+    oldApproximation[yNods][j] = uGrid[yNods][j]
 for i in range(yNods + 1):
-    oldApproximation[i][0] = uFunctionGrid[i][0]
-    oldApproximation[i][xNods] = uFunctionGrid[i][xNods]
+    oldApproximation[i][0] = uGrid[i][0]
+    oldApproximation[i][xNods] = uGrid[i][xNods]
 fistApproximation = oldApproximation.copy()
 newApproximation = np.zeros((yNods + 1, xNods + 1))
 
-
-while not terminationMethod(fistApproximation, newApproximation, uFunctionGrid, eps):
+while not terminationMethod(fistApproximation, newApproximation, uGrid, eps):
     numberOfIterations += 1
     for i in range(1, yNods):
         for j in range(1, xNods):
@@ -53,21 +74,8 @@ while not terminationMethod(fistApproximation, newApproximation, uFunctionGrid, 
         for j in range(xNods + 1):
             oldApproximation[i][j] = newApproximation[i][j]
 
-LuResultGrid = np.zeros((yNods + 1, xNods + 1))
-for i in range(1, yNods):
-    for j in range(1, xNods):
-        LuResultGrid[i][j] = (newApproximation[i + 1][j] - newApproximation[i][j]) / (step ** 2) - \
-                       (newApproximation[i][j] - newApproximation[i - 1][j]) / (step ** 2) + \
-                       (newApproximation[i][j + 1] - newApproximation[i][j]) / (step ** 2) - \
-                       (newApproximation[i][j] - newApproximation[i][j - 1]) / (step ** 2)
-LuFirstGrid = np.zeros((yNods + 1, xNods + 1))
-for i in range(1, yNods):
-    for j in range(1, xNods):
-        LuFirstGrid[i][j] = (fistApproximation[i + 1][j] - fistApproximation[i][j]) / (step ** 2) - \
-                       (fistApproximation[i][j] - fistApproximation[i - 1][j]) / (step ** 2) + \
-                       (fistApproximation[i][j + 1] - fistApproximation[i][j]) / (step ** 2) - \
-                       (fistApproximation[i][j] - fistApproximation[i][j - 1]) / (step ** 2)
-
+LuResultGrid = LuGrid(newApproximation, step)
+LuFirstGrid = LuGrid(fistApproximation, step)
 
 print("Iteration method with optimal parameter. Variant 6.")
 print("eps:", eps, "\n")
@@ -75,6 +83,3 @@ print("eps:", eps, "\n")
 print("Measure of approximation.  ||F-AU_*||:", np.max(np.abs(LuResultGrid[1:-1, 1:-1] + fFunctionGrid[1:-1, 1:-1])))
 print("Discrepancy norm for U^0.  ||F-AU^0||:", np.max(np.abs(LuFirstGrid[1:-1, 1:-1] + fFunctionGrid[1:-1, 1:-1])))
 print("Number of iterations:", numberOfIterations)
-
-
-
