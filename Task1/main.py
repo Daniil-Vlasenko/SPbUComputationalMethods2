4365f77ab0a08c002609281147c5c1fb07cc571c
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2022-09-27 08:09:08 +0300

    .

--- Task1/main.py
+++ Task1/main.py
@@ -14,6 +14,8 @@ class IterationMethodWithOptimalParameter:
         self.step = self.xStep  # Убрать это
         self.firstApproximation = None
         self.lastApproximation = None
+        self.uGridMatrix = None
+        self.fGridMatrix = None
 
     def uFunction(self, x, y):
         return 2 * x ** 3 * y ** 3
@@ -43,26 +45,26 @@ class IterationMethodWithOptimalParameter:
         return LuResultGrid
 
     def iterationMethodWithOptimalParameter(self):
-        uGridMatrix = self.uGrid(self.xNods, self.yNods)
-        fGridMatrix = self.fGrid(self.xNods, self.yNods)
+        self.uGridMatrix = self.uGrid(self.xNods, self.yNods)
+        self.fGridMatrix = self.fGrid(self.xNods, self.yNods)
 
         oldApproximation = np.zeros((self.yNods + 1, self.xNods + 1))
         for j in range(self.xNods + 1):
-            oldApproximation[0][j] = uGridMatrix[0][j]
-            oldApproximation[self.yNods][j] = uGridMatrix[self.yNods][j]
+            oldApproximation[0][j] = self.uGridMatrix[0][j]
+            oldApproximation[self.yNods][j] = self.uGridMatrix[self.yNods][j]
         for i in range(self.yNods + 1):
-            oldApproximation[i][0] = uGridMatrix[i][0]
-            oldApproximation[i][self.xNods] = uGridMatrix[i][self.xNods]
+            oldApproximation[i][0] = self.uGridMatrix[i][0]
+            oldApproximation[i][self.xNods] = self.uGridMatrix[i][self.xNods]
         self.firstApproximation = oldApproximation.copy()
         self.lastApproximation = np.zeros((self.yNods + 1, self.xNods + 1))
 
-        while not self.terminationMethod(uGridMatrix):
+        while not self.terminationMethod(self.uGridMatrix):
             self.numberOfIterations += 1
             for i in range(1, self.yNods):
                 for j in range(1, self.xNods):
                     self.lastApproximation[i][j] = (oldApproximation[i - 1][j] + oldApproximation[i + 1][j] +
                                               oldApproximation[i][j - 1] + oldApproximation[i][j + 1] +
-                                              self.step ** 2 * fGridMatrix[i][j]) / 4
+                                              self.step ** 2 * self.fGridMatrix[i][j]) / 4
             for j in range(self.xNods + 1):
                 self.lastApproximation[0][j] = oldApproximation[0][j]
                 self.lastApproximation[self.yNods][j] = oldApproximation[self.yNods][j]
@@ -73,15 +75,16 @@ class IterationMethodWithOptimalParameter:
                 for j in range(self.xNods + 1):
                     oldApproximation[i][j] = self.lastApproximation[i][j]
 
-        LuResultGrid = self.LuGrid(self.lastApproximation, self.step) 
-        LuFirstGrid = self.LuGrid(self.firstApproximation, self.step)
-
-        print("Iteration method with optimal parameter. Variant 6.")
-        print("eps:", self.eps, "\n")
-
-        print("Measure of approximation.  ||F-AU_*||:", np.max(np.abs(LuResultGrid[1:-1, 1:-1] + fGridMatrix[1:-1, 1:-1])))
-        print("Discrepancy norm for U^0.  ||F-AU^0||:", np.max(np.abs(LuFirstGrid[1:-1, 1:-1] + fGridMatrix[1:-1, 1:-1])))
-        print("Number of iterations:", self.numberOfIterations)
 
 method = IterationMethodWithOptimalParameter(0.001, 1, 1, 3, 3)
 method.iterationMethodWithOptimalParameter()
+
+LuResultGrid = method.LuGrid(method.lastApproximation, method.step)
+LuFirstGrid = method.LuGrid(method.firstApproximation, method.step)
+
+print("Iteration method with optimal parameter. Variant 6.")
+print("eps:", method.eps, "\n")
+
+print("Measure of approximation.  ||F-AU_*||:", np.max(np.abs(LuResultGrid[1:-1, 1:-1] + method.fGridMatrix[1:-1, 1:-1])))
+print("Discrepancy norm for U^0.  ||F-AU^0||:", np.max(np.abs(LuFirstGrid[1:-1, 1:-1] + method.fGridMatrix[1:-1, 1:-1])))
+print("Number of iterations:", method.numberOfIterations)
