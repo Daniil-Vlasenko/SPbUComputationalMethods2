5b5ce1411edf4c7b8ae2a0135244f4afa2d433a4
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2022-10-14 15:47:10 +0300

    .

--- Task2/main.py
+++ Task2/main.py
@@ -1,10 +1,11 @@
 import math
 import numpy as np
-
+import pandas as pd
 
 np.set_printoptions(linewidth=np.inf)
 np.set_printoptions(precision=5)
 np.set_printoptions(suppress=True)
+
 
 class ZeidelMethod:
     def __init__(self, eps, xLength, yLength, xNods, yNods):
@@ -16,14 +17,17 @@ class ZeidelMethod:
         self.xStep = xLength / xNods
         self.yStep = yLength / yNods
         self.numberOfIterations = 0
-        self.step = self.xStep  # Убрать это
+        self.step = self.xStep  # !
         self.firstApproximation = None
         self.lastApproximation = None
-        self.uGridMatrix = None
-        self.fGridMatrix = None
-        self.eigenvalueMin = None
-        self.eigenvalueMax = None
-        self.spectralRadius = None
+        self.uGridMatrix = self.uGrid(self.xNods, self.yNods)
+        self.fGridMatrix = self.fGrid(self.xNods, self.yNods)
+        self.eigenvalueMin = 8 / (self.step * self.step) * math.sin(math.pi * self.step / 2) * math.sin(
+            math.pi * self.step / 2)
+        self.eigenvalueMax = 8 / (self.step * self.step) * math.cos(math.pi * self.step / 2) * math.cos(
+            math.pi * self.step / 2)
+        self.spectralRadius = (self.eigenvalueMax - self.eigenvalueMin) / (self.eigenvalueMax + self.eigenvalueMin)
+        self.informationTable = pd.DataFrame(columns=["rel.error", "sp.rad._k"])
 
     def uFunction(self, x, y):
         return 2 * x ** 3 * y ** 3
@@ -31,8 +35,9 @@ class ZeidelMethod:
     def fFunction(self, x, y):
         return -12 * x * y * (y ** 2 + x ** 2)
 
-    def terminationMethod(self, uGridMatrix):
-        return np.max(np.abs(self.lastApproximation - uGridMatrix)) / np.max(np.abs(self.firstApproximation - uGridMatrix)) < self.eps
+    def terminationMethod(self):
+        return np.max(np.abs(self.lastApproximation - self.uGridMatrix)) / np.max(
+            np.abs(self.firstApproximation - self.uGridMatrix)) < self.eps
 
     def uGrid(self, xNods, yNods):
         return np.array([[self.uFunction(i / xNods, j / yNods) for j in range(yNods + 1)] for i in range(xNods + 1)])
@@ -52,18 +57,7 @@ class ZeidelMethod:
                                      (uGridMatrix[i][j] - uGridMatrix[i][j - 1]) / (step ** 2)
         return LuResultGrid
 
-    # Функция определена для простейшего случая.
-    def eigenvalueCalculation(self):
-        self.eigenvalueMin = 8 / (self.step * self.step) * math.sin(math.pi * self.step / 2) * math.sin(
-            math.pi * self.step / 2)
-        self.eigenvalueMax = 8 / (self.step * self.step) * math.cos(math.pi * self.step / 2) * math.cos(
-            math.pi * self.step / 2)
-
-    def spectralRadiusCalculation(self):
-        self.eigenvalueCalculation()
-        self.spectralRadius = (self.eigenvalueMax - self.eigenvalueMin) / (self.eigenvalueMax + self.eigenvalueMin)
-
-    def zeidelMethod(self):
+    def ZeidelMethod(self):
         self.uGridMatrix = self.uGrid(self.xNods, self.yNods)
         self.fGridMatrix = self.fGrid(self.xNods, self.yNods)
 
@@ -77,31 +71,50 @@ class ZeidelMethod:
         self.firstApproximation = oldApproximation.copy()
         self.lastApproximation = np.zeros((self.yNods + 1, self.xNods + 1))
 
-        while not self.terminationMethod(self.uGridMatrix):
+        while not self.terminationMethod():
             self.numberOfIterations += 1
             for i in range(1, self.yNods):
                 for j in range(1, self.xNods):
                     self.lastApproximation[i][j] = (self.lastApproximation[i - 1][j] + oldApproximation[i + 1][j] +
-                                              self.lastApproximation[i][j - 1] + oldApproximation[i][j + 1] +
-                                              self.step ** 2 * self.fGridMatrix[i][j]) / 4
+                                                    self.lastApproximation[i][j - 1] + oldApproximation[i][j + 1] +
+                                                    self.step ** 2 * self.fGridMatrix[i][j]) / 4
             for j in range(self.xNods + 1):
                 self.lastApproximation[0][j] = oldApproximation[0][j]
                 self.lastApproximation[self.yNods][j] = oldApproximation[self.yNods][j]
             for i in range(self.yNods + 1):
                 self.lastApproximation[i][0] = oldApproximation[i][0]
                 self.lastApproximation[i][self.xNods] = oldApproximation[i][self.xNods]
-            for i in range(self.yNods + 1):
-                for j in range(self.xNods + 1):
-                    oldApproximation[i][j] = self.lastApproximation[i][j]
+
+            # Формирование вывода.
+            rel_error = np.max(np.abs(self.lastApproximation - self.uGridMatrix)) / \
+                        np.max(np.abs(self.firstApproximation - self.uGridMatrix))
+            p_k1, p_k2 = 0, 0
+            if self.numberOfIterations < 3:
+                self.informationTable.loc[len(self.informationTable.index)] = [rel_error, None]
+            else:
+                p_k2 = p_k1
+                p_k1 = np.max(np.abs(self.lastApproximation - oldApproximation)) / \
+                       np.max(np.abs(oldApproximation - oldOldApproximation))
+                if self.eigenvalueMax == - self.eigenvalueMin:
+                    if self.numberOfIterations == 3:
+                        self.informationTable.loc[len(self.informationTable.index)] = [rel_error, None]
+                    else:
+                        self.informationTable.loc[len(self.informationTable.index)] = [rel_error, (p_k1 * p_k2) ** (1/2)]
+                else:
+                    self.informationTable.loc[len(self.informationTable.index)] = [rel_error, p_k1]
+            # Формирование вывода.
+
+            oldOldApproximation = oldApproximation.copy()
+            oldApproximation = self.lastApproximation.copy()
 
 
-method = ZeidelMethod(0.0001, 1, 1, 5, 5)
-method.zeidelMethod()
+method = ZeidelMethod(0.001, 1, 1, 5, 5)
+method.ZeidelMethod()
 
 LuResultGrid = method.LuGrid(method.uGridMatrix, method.step)
 LuFirstGrid = method.LuGrid(method.firstApproximation, method.step)
 
-print("Seidel Method. Variant 6.")
+print("Iteration method withOptimal parameter. Variant 6.")
 print("eps:", method.eps, "\n")
 
 print("Measure of approximation.  ||F-AU_*||:",
@@ -109,8 +122,9 @@ print("Measure of approximation.  ||F-AU_*||:",
 print("Discrepancy norm for U^0.  ||F-AU^0||:",
       np.max(np.abs(LuFirstGrid[1:-1, 1:-1] + method.fGridMatrix[1:-1, 1:-1])))
 print("Number of iterations:", method.numberOfIterations)
-method.spectralRadiusCalculation()
-print("Spectral radius:", method.spectralRadius)
+print("(Spectral radius)^2:", method.spectralRadius ** 2)
+
+print("\nTable 1\n", method.informationTable)
 
 print("\nApproximate solution:")
 print(method.lastApproximation)
